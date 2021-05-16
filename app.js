49950d549b0cd091a89af6c738fcc1a7e04cf4ce
bharatkandpal <36111716+bharatkandpal@users.noreply.github.com>
2021-05-17 01:46:48 +0530

    pages added for success and failure

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const request = require('request')
+const https = require('https')
 
 const app = express()
 app.use(express.static("./"))
@@ -14,8 +15,51 @@ app.post('/signup', (req, res) => {
     const fName = req.body.fname
     const lName = req.body.lname
     const eMail = req.body.email
-    console.log(req.body);
-    res.status(201).send(fName+" "+lName+" "+eMail+" ")
+
+    const data = {
+        members: [
+            {
+                email_address: eMail,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fName,
+                    LNAME: lName
+                }
+            }
+        ]
+
+    }
+    const jsonData = JSON.stringify(data)
+    // API KEY
+    // mailchimp
+    const key = 'aabc85ceac713cc70247937f1f6bbc18-us1'
+    // list_id
+    const list_id = 'd72cd954b9'
+    const url = 'https://us1.api.mailchimp.com/3.0/lists/'+list_id
+    const options = {
+        method: "POST",
+        auth:"bk:aabc85ceac713cc70247937f1f6bbc18-us1"
+
+    }
+    const request = https.request(url, options, (resp) => {
+        console.log(resp.statusCode)
+        if(resp.statusCode==200)
+            res.sendFile(__dirname+"\\success.html")
+        else
+            res.sendFile(__dirname+"\\failure.html")
+
+        
+        resp.on("data",(data)=>{
+            console.log(JSON.parse(data))
+        })
+
+    })
+    request.write(jsonData)
+    request.end()
+})
+
+app.post("/failure",(req,res)=>{
+    res.redirect("/home")
 })
 
 app.listen(3005, () => {
@@ -23,3 +67,7 @@ app.listen(3005, () => {
 })
 
 
+
+// https://${dc}.api.mailchimp.com/3.0/lists \
+//   --user "anystring:${apikey}"' \
+//   -d '{"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}'
